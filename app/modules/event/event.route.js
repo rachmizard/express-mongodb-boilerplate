768f0ec0256d799eed4b89de89c404a58bd18dc1
Figur <@DESKTOP-EG2IU91>
2023-02-27 22:10:09 +0700

    add middleware

--- app/modules/event/event.route.js
+++ app/modules/event/event.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { CatchAsyncMiddleware, ValidatorMiddleware } from "core";
+import {
+  AuthMiddleware,
+  CatchAsyncMiddleware,
+  ValidatorMiddleware,
+} from "core";
 
 import EventController from "./event.controller";
 import { eventValidators } from "./event.validator";
@@ -10,20 +14,23 @@ eventRoutes.get("/", CatchAsyncMiddleware.handle(EventController.index));
 eventRoutes.get("/:id", CatchAsyncMiddleware.handle(EventController.show));
 
 eventRoutes.post(
-	"/",
-	ValidatorMiddleware.handle(eventValidators),
-	CatchAsyncMiddleware.handle(EventController.create)
+  "/",
+  AuthMiddleware.handle,
+  ValidatorMiddleware.handle(eventValidators),
+  CatchAsyncMiddleware.handle(EventController.create)
 );
 
 eventRoutes.put(
-	"/:id",
-	ValidatorMiddleware.handle(eventValidators),
-	CatchAsyncMiddleware.handle(EventController.update)
+  "/:id",
+  AuthMiddleware.handle,
+  ValidatorMiddleware.handle(eventValidators),
+  CatchAsyncMiddleware.handle(EventController.update)
 );
 
 eventRoutes.delete(
-	"/:id",
-	CatchAsyncMiddleware.handle(EventController.delete)
+  "/:id",
+  AuthMiddleware.handle,
+  CatchAsyncMiddleware.handle(EventController.delete)
 );
 
 export default eventRoutes;
