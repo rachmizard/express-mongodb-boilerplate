a9fd5b6f51f3d6c5dd2970cefcbf02a1bb031168
muhrizram <muhrizram19@gmail.com>
2023-02-27 09:11:24 +0700

    Add Auth

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,12 +1,40 @@
+import { AuthService, TokenService, UserService } from "../services";
 import ApiResponse from "../utils/response/ApiResponse";
 
-export default class AuthController {
+class AuthController {
 	constructor() {}
 
-	static signIn(req, res) {
+	static async signIn(req, res) {
+		const {email, password} = req.body;
+		const user = await AuthService.signIn(email, password);
+		const token = TokenService.generateToken(user);
+
 		return ApiResponse.sendOk({
 			// add some properties below
 			message: "Sign in successfully",
+			data: {
+				token
+			}
+		})(res);
+	}
+
+	static async signUp(req, res) {
+		const {email, password, username} = req.body;
+		const user = await UserService.createUser({
+			email,
+			password,
+			username
+		});
+
+		const token = TokenService.generateToken(user);
+		return ApiResponse.sendOk({
+			// add some properties below
+			message: "Sign up successfully",
+			data: {
+				token
+			}
 		})(res);
 	}
 }
+
+export default AuthController;
--- app/routes/auth.route.js
+++ app/routes/auth.route.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-
 import AuthController from "../controllers/AuthController";
+import AuthMiddleware from "../middlewares/Auth";
 import CatchAsyncMiddleware from "../middlewares/CatchAsync";
+import Validator from "../middlewares/Validator";
+import { signInValidators, signUpValidators } from "../validators/auth.validator";
 
 const authRoutes = Router();
 
-authRoutes.post("/signin", CatchAsyncMiddleware.handle(AuthController.signIn));
+authRoutes.post("/signin", Validator.validate(signInValidators), CatchAsyncMiddleware.handle(AuthController.signIn));
+
+authRoutes.post("/signup", Validator.validate(signUpValidators), CatchAsyncMiddleware.handle(AuthController.signUp));
+
+authRoutes.get("/profile", AuthMiddleware.handle, (req, res) => {
+    res.json({message: "You are authenticated", user: req.user});
+});
 
 export default authRoutes;
